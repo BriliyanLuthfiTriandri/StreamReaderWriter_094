5b1709361392a32f5e3158d8f92160cd5ee76562
BriliyanLuthfiTriandri <briliyanlt@gmail.com>
2023-06-21 11:02:07 +0700

    mendisplay pada layar dan unutk unlimited loop untuk menulis

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -14,4 +14,11 @@ int main() {
 	ofstream outfile;
 	//menunjukan ke sebuah nama file 
 	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+	}
 }  
